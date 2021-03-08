839b09880c3a9963ab8030789c99901fdcfc6891
Sanjay <sanjayjaras@gmail.com>
2021-03-07 20:33:14 -0600

    with polybar

--- config.h
+++ config.h
@@ -1,20 +1,16 @@
 /* See LICENSE file for copyright and license details. */
-
-
 /** Added for volume hardware keys **/
 #include <X11/XF86keysym.h>
-
-
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 8;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
+static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
+static const char *alttrayname      = "tray";    /* Polybar tray instance name */
+static const char *altbarcmd        = "$HOME/.config/polybar/launcher.sh"; /* Alternate bar launch command */
 static const char *fonts[]          = {"Cascadia Code Pl-bold:size=10:antialias=true:autohint=true:style=Bold",
 									   "Font Awesome 5 Free Solid:size=10:antialias=true:autohint=true:style=Bold"};
 static const char dmenufont[]       = "Cascadia Code Pl-bold:size=10";
@@ -24,29 +20,14 @@ static const char col_gray3[]       = "#dddddd";
 static const char col_gray4[]       = "#fefefe";
 static const char col_cyan[]        = "#454567";//"#005577";
 
-static const char col_black[]       = "#282c34";
-static const char col_red[]         = "#ec5a9d";
-static const char col_blue[]      	= "#4cdbe0";
-static const char col_white[]       = "#ffffff";
-
 static const char active_border[]   = "#bc0a4d";
 static const char inactive_border[] = "#005577";
 
-
-//Added extra colors to work with status commands and status colors
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	
-	[Scheme1] = { col_blue, col_gray1,  active_border  },
-	[Scheme2] = { col_red, col_gray1, inactive_border },
-	[Scheme3] =	{ col_blue, col_gray1, col_gray1 },
-	[Scheme4] = { col_red, col_gray1, col_gray1 },
-	[Scheme5] = { col_blue, col_gray1,  col_gray1  },
-	[Scheme6] =	{ col_red, col_gray1, col_gray1 },
-	[Scheme7] =	{ col_blue, col_gray1, col_gray1 },
-	[Scheme8] = { col_red, col_gray1, col_gray1 },
+	[SchemeNorm] = { col_gray3, col_gray1, inactive_border },
+	[SchemeSel]  = { col_gray4, col_cyan,  active_border  },
 };
-
 
 /* tagging */
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -90,7 +71,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *dmenucmd[] = {"dmenu_run", NULL};
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char *rofi_dmenu[]  = { "/home/sanjay/.config/rofi/launchers/text/launcher_1.sh", NULL };
 static const char *rofi[]  = { "/home/sanjay/.config/rofi/launchers/misc/launcher.sh", NULL };
 static const char *logout[]  = { "oblogout", NULL };
@@ -104,18 +85,6 @@ static const char *medplay[] = { "playerctl", "play-pause",  NULL };
 static const char *mednext[] = { "playerctl", "next",  NULL };
 static const char *medprev[] = { "playerctl", "play-previous",  NULL };
 static const char *medstop[] = { "playerctl", "stop",  NULL };
-
-
-/* commands spawned when clicking statusbar, the mouse button pressed is exported as BUTTON */
-static char *statuscmds[] = { "~/applications/dwmblocks/scripts/cpu $BUTTON", 
-							 "~/applications/dwmblocks/scripts/memory $BUTTON",
-							 "~/applications/dwmblocks/scripts/gpu-temp $BUTTON", 
-							 "~/applications/dwmblocks/scripts/net $BUTTON", 
-							 "~/applications/dwmblocks/scripts/volume $BUTTON", 
-							 "~/applications/dwmblocks/scripts/pacupdate $BUTTON", 
-							 "~/applications/dwmblocks/scripts/clock $BUTTON",
-							 "oblogout" };
-static char *statuscmd[] = { "/bin/sh", "-c", NULL, NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -183,9 +152,7 @@ static Button buttons[] = {
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
-	{ ClkStatusText,        0,              Button1,        spawn,          {.v = statuscmd } },
-	{ ClkStatusText,        0,              Button2,        spawn,          {.v = statuscmd } },
-	{ ClkStatusText,        0,              Button3,        spawn,          {.v = statuscmd } },
+	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
 	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
@@ -193,5 +160,23 @@ static Button buttons[] = {
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+};
+
+static const char *ipcsockpath = "/tmp/dwm.sock";
+static IPCCommand ipccommands[] = {
+  IPCCOMMAND(  view,                1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  toggleview,          1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  tag,                 1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  toggletag,           1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  tagmon,              1,      {ARG_TYPE_UINT}   ),
+  IPCCOMMAND(  focusmon,            1,      {ARG_TYPE_SINT}   ),
+  IPCCOMMAND(  focusstack,          1,      {ARG_TYPE_SINT}   ),
+  IPCCOMMAND(  zoom,                1,      {ARG_TYPE_NONE}   ),
+  IPCCOMMAND(  incnmaster,          1,      {ARG_TYPE_SINT}   ),
+  IPCCOMMAND(  killclient,          1,      {ARG_TYPE_SINT}   ),
+  IPCCOMMAND(  togglefloating,      1,      {ARG_TYPE_NONE}   ),
+  IPCCOMMAND(  setmfact,            1,      {ARG_TYPE_FLOAT}  ),
+  IPCCOMMAND(  setlayoutsafe,       1,      {ARG_TYPE_PTR}    ),
+  IPCCOMMAND(  quit,                1,      {ARG_TYPE_NONE}   )
 };
 
